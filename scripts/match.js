06f3657b59b029ada7a28eb6cd99e42b1474f859
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-01-04 20:24:07 +0330

    improve match logic

--- scripts/match.js
+++ scripts/match.js
@@ -1,30 +1,43 @@
 const nimFasele = "‌";
 
+const lsp = (s1, s2) => {
+    let pv = [];
+    for (let j = 0; j <= s2.length; j += 1) {
+        pv.push(0);
+    }
+    for (let i = 0; i < s1.length; i += 1) {
+        const dp = [0];
+        for (let j = 0; j < s2.length; j += 1) {
+            if (s1[i] === s2[j]) {
+                dp.push(pv[j] + 1);
+            } else {
+                dp.push(Math.max(dp[j], pv[j + 1]));
+            }
+        }
+        pv = dp;
+    }
+    return pv[s2.length];
+};
+
+
+const match = (s1, s2) => {
+    if (s1 === s2) {
+        return true;
+    }
+    if (s1.endsWith('ه') && s2 === s1 + nimFasele + 'ی') {
+        return true;
+    }
+    if (s2.endsWith('ه') && s1 === s2 + nimFasele + 'ی') {
+        return true;
+    }
+    return false;
+};
+
 const doFile = async (fnum) => {
     const json = JSON.parse(await Deno.readTextFile(`vosk_output/${fnum}.json`));
     const text = await Deno.readTextFile(`texts/${fnum}.txt`);
     const list = text.replaceAll('،', '').replaceAll('.', '').split(/\s+/);
     const { result } = json;
-    
-    const lsp = (s1, s2) => {
-        let pv = [];
-        for (let j = 0; j <= s2.length; j += 1) {
-            pv.push(0);
-        }
-        for (let i = 0; i < s1.length; i += 1) {
-            const dp = [0];
-            for (let j = 0; j < s2.length; j += 1) {
-                if (s1[i] === s2[j]) {
-                    dp.push(pv[j] + 1);
-                } else {
-                    dp.push(Math.max(dp[j], pv[j + 1]));
-                }
-            }
-            pv = dp;
-        }
-        return pv[s2.length];
-    };
-    
     {
         let i = 0;
         let j = 0;
@@ -33,70 +46,75 @@ const doFile = async (fnum) => {
                 break;
             }
             let finded = false;
-            if (!finded) {
-                for (let x = 1; x < 6; x += 1) {
-                    if (i + x - 1 >= result.length) continue;
-                    if (j + x - 1 >= list.length) continue;
-                    if (result[i + x - 1].word === list[j + x - 1]) {
-                        for (let f = 0; f < x; f += 1) {
-                            result[i + f].corrected = list[j + f];
-                        }
-                        i += x;
-                        j += x;
-                        finded = true;
-                        break;
-                    }
-                    if (result[i + x - 1].word === list[j + x - 1] + nimFasele + list[j + x]) {
-                        for (let f = 0; f < x; f += 1) {
-                            result[i + f].corrected = list[j + f];
-                        }
-                        result[i + x - 1].corrected += nimFasele + list[j + x];
-                        i += x;
-                        j += x + 1;
-                        finded = true;
-                        break;
-                    }
-                }
-            }
-            for (let y = 1; y < 5; y += 1) {
+            for (let l = 3; l < 8; l += 1) {
                 if (finded) {
                     break;
                 }
-                for (let x = 0; x < 5; x += 1) {
-                    if (j + x + y >= list.length) continue;
-                    if (i + x + y >= result.length) continue;
-                    if (result[i + x].word === list[j + x + y]) {
-                        for (let f = 0; f < x; f += 1) {
-                            result[i + f].corrected = list[j + f];
-                        }
-                        if (x !== 0) {
-                            for (let f = 0; f < y; f += 1) {
-                                result[i + x - 1].corrected += ' ' + list[j + x + f];
+                if (!finded) {
+                    for (let x = 1; x < l; x += 1) {
+                        if (i + x - 1 >= result.length) continue;
+                        if (j + x - 1 >= list.length) continue;
+                        if (match(result[i + x - 1].word, list[j + x - 1])) {
+                            for (let f = 0; f < x; f += 1) {
+                                result[i + f].corrected = list[j + f];
                             }
-                            result[i + x].corrected = list[j + x + y];    
-                        } else {
-                            result[i + x].corrected = list[j + x];
-                            for (let f = 1; f <= y; f += 1) {
-                                result[i + x].corrected += ' ' + list[j + x + f];
-                            }
+                            i += x;
+                            j += x;
+                            finded = true;
+                            break;
                         }
-                        i += x + 1;
-                        j += x + y + 1;
-                        finded = true;
+                        if (match(result[i + x - 1].word, list[j + x - 1] + nimFasele + list[j + x])) {
+                            for (let f = 0; f < x; f += 1) {
+                                result[i + f].corrected = list[j + f];
+                            }
+                            result[i + x - 1].corrected += nimFasele + list[j + x];
+                            i += x;
+                            j += x + 1;
+                            finded = true;
+                            break;
+                        }
+                    }
+                }
+                for (let y = 1; y < l; y += 1) {
+                    if (finded) {
                         break;
                     }
-                    if (result[i + x + y].word === list[j + x]) {
-                        for (let f = 0; f < x; f += 1) {
-                            result[i + f].corrected = list[j + f];
+                    for (let x = 0; x + y < l; x += 1) {
+                        if (j + x + y >= list.length) continue;
+                        if (i + x + y >= result.length) continue;
+                        if (match(result[i + x].word, list[j + x + y])) {
+                            for (let f = 0; f < x; f += 1) {
+                                result[i + f].corrected = list[j + f];
+                            }
+                            if (x !== 0) {
+                                for (let f = 0; f < y; f += 1) {
+                                    result[i + x - 1].corrected += ' ' + list[j + x + f];
+                                }
+                                result[i + x].corrected = list[j + x + y];    
+                            } else {
+                                result[i + x].corrected = list[j + x];
+                                for (let f = 1; f <= y; f += 1) {
+                                    result[i + x].corrected += ' ' + list[j + x + f];
+                                }
+                            }
+                            i += x + 1;
+                            j += x + y + 1;
+                            finded = true;
+                            break;
                         }
-                        for (let f = 0; f < y; f += 1) {
-                            result[i + x + f].skip = true;
+                        if (match(result[i + x + y].word, list[j + x])) {
+                            for (let f = 0; f < x; f += 1) {
+                                result[i + f].corrected = list[j + f];
+                            }
+                            for (let f = 0; f < y; f += 1) {
+                                result[i + x + f].skip = true;
+                            }
+                            result[i + x + y].corrected = list[j + x];
+                            i += x + y + 1;
+                            j += x + 1;
+                            finded = true;
+                            break;
                         }
-                        result[i + x + y].corrected = list[j + x];
-                        i += x + y + 1;
-                        j += x + 1;
-                        finded = true;
-                        break;
                     }
                 }
             }
